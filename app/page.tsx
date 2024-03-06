3d0a2089b8e0da0e110ae7517dcaa9ce0d0486e1
Amine <mezzatamine2002@gmail.com>
2024-03-06 09:55:33 +0100

    alo

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,16 @@ import { useEffect } from "react";
 import Image from "next/image";
 import Hero from "@/app/ui/home/hero"
 import Aboutme from "@/app/ui/home/aboutme"
-import Services from "@/app/ui/home/services"
+import Services from "@/app/ui/home/footer"
 import { Parallax } from 'react-scroll-parallax';
+import Footer from "./ui/home/footer";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Parallax speed={-30} ><Hero></Hero></Parallax>
       <Parallax speed={40} easing={"easeIn"}><Aboutme></Aboutme></Parallax>
-      <Services></Services>
+      <Parallax speed={8} easing={"easeIn"}><Footer></Footer></Parallax>
      </main>
   );
 }
---
+++ app/ui/home/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer(){
+    return (
+        <div className="relative min-h-screen w-full">
+            <section className="flex flex-col rounded-lg pb-20 overflow-x-clip">
+                <h1 className="companim text-9xl italic font-bold pb-0 pl-44 pt-11">
+                    GET IN TOUCH
+                </h1>
+            </section>
+        </div>
+    )
+}
